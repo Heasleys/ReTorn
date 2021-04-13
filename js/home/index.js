23cebd54182d0a7df623fff0b7328c1ef411dff6
Heasley <heasleys4hemp@hotmail.com>
2021-04-13 01:03:40 -0400

    added effective stats to homepage

--- js/home/index.js
+++ js/home/index.js
@@ -4,3 +4,69 @@
 
 let totPP = $('div#personal-perks').find('ul > li.last').text().replace(/\D/g,'');
 $('h5.box-title:contains("Personal Perks")').text( 'Personal Perks: ' + totPP).prop('title', 'Total Personal Perks: ' + totPP);
+
+
+var bsBox = $('h5.box-title:contains("Battle Stats")').parent('div.title').parent('div.sortable-box');
+var statsContainer = bsBox.children('div.bottom-round');
+
+bsBox.find('div.battle.bottom-round').removeClass('bottom-round');
+var stats = [];
+var bonuses = [];
+bsBox.find('ul > li').each( function(i) {
+  let stat = parseInt($(this).find('span.desc').text().replace(/\D/g,''));
+  let perc = parseInt($(this).find('span.mod-value').text());
+  let bonus = (stat * (perc / 100));
+  let calced = stat + bonus;
+
+  stats[i] = Math.floor(calced);
+  bonuses[i] = Math.floor(bonus);
+});
+stats[4] = Math.floor(stats[0] + stats[1] + stats[2] + stats[3]);
+bonuses[4] = Math.floor(bonuses[0] + bonuses[1] + bonuses[2] + bonuses[3]);
+
+statsContainer.append(`
+  <div class="re_head expanded flat-top">
+    <span class="re_title">ReTorn: <span id="re_title">Effective Stats</span></span>
+  </div>
+  <div class="re_stats_list">
+
+    <ul>
+    <li tabindex="0" aria-label="Strength: `+stats[0].toLocaleString()+`">
+      <span class="re_divider">
+        <span>Strength</span>
+      </span>
+      <span class="stats" title="`+bonuses[0].toLocaleString()+`">`+stats[0].toLocaleString()+`</span>
+    </li>
+
+    <li tabindex="0" role="row" aria-label="Defense: `+stats[1].toLocaleString()+`">
+      <span class="re_divider">
+        <span>Defense</span>
+      </span>
+      <span class="stats" title="`+bonuses[1].toLocaleString()+`">`+stats[1].toLocaleString()+`</span>
+    </li>
+
+    <li tabindex="0" role="row" aria-label="Speed: `+stats[2].toLocaleString()+`">
+      <span class="re_divider">
+        <span>Speed</span>
+      </span>
+      <span class="stats" title="`+bonuses[2].toLocaleString()+`">`+stats[2].toLocaleString()+`</span>
+    </li>
+
+    <li tabindex="0" role="row" aria-label="Dexterity: `+stats[3].toLocaleString()+`">
+      <span class="re_divider">
+      <span>Dexterity</span>
+      </span>
+      <span class="stats" title="`+bonuses[3].toLocaleString()+`">`+stats[3].toLocaleString()+`</span>
+    </li>
+
+    <li class="last" tabindex="0" role="row" aria-label="Total: 4,205,666,307">
+      <span class="re_divider">
+      <span>Total</span>
+      </span>
+      <span class="stats" title="`+bonuses[4].toLocaleString()+`">`+stats[4].toLocaleString()+`</span>
+    </li>
+
+    </ul>
+
+  </div>
+  `);
