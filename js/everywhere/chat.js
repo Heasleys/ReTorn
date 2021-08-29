1a6077729699ce71472ea01b466f03d0e5d3130d
Heasley <heasleys4hemp@hotmail.com>
2021-08-29 18:19:31 -0400

    Fix bug where chat highlights only worked when trade chat exists

--- js/everywhere/chat.js
+++ js/everywhere/chat.js
@@ -1,98 +1,106 @@
 (function() {
-  function insertChatSearch() {
-    $('div[class^="chat-box-head"] > div[class^="chat-box-title"][title="Trade"]').append(`
-    <input type="text" class="re_chat_search" title="Filter Chat">
-    `);
 
-    $('input.re_chat_search').click(function() {
-      event.stopPropagation();
-    });
-
-    $('input.re_chat_search').on('input', function() {
-      var v = $(this).val().toLowerCase();
-      var chats = $('div[class^="chat-box"][class*="trade"]').find('div[class^="overview"] > div[class^="message"]');
-      chats.each(function() {
-        if (v == "") {
-          $(this).show();
-        } else {
-          let message = $(this).find('span').text().toLowerCase();
-          if (!message.includes(v)) {
-            $(this).hide();
-          } else {
-            $(this).show();
-          }
-        }
-      });
-
-    });
-  }
-
-  //Start Chat Functions - Add Filter Textbox/Check Highlights
-  const observer = new MutationObserver(function(mutations) {
-    if ($('div[class^="chat-box"][class*="trade"]').length != 0 && $('input.re_chat_search').length == 0 && $('#chatRoot').length == 1) {
+//Create Observer for start of Chat Functions - Add Filter Textbox/Check Highlights
+const observer = new MutationObserver(function(mutations) {
+  if ($('#chatRoot').length == 1) {
+    if ($('div[class^="chat-box"][class*="trade"]').length != 0 && $('input.re_chat_search').length == 0) {
       insertChatSearch();
-      let chatRoot = document.getElementById("chatRoot");
-      chatobserver.observe(chatRoot, {attributes: false, childList: true, characterData: false, subtree:true});
-      highlightAllNames();
-      nameHighlightObserver.observe(chatRoot, {attributes: false, childList: true, characterData: false, subtree:true});
-      observer.disconnect();
     }
-  });
+    //Start other observers
+    const chatRoot = document.getElementById("chatRoot");
+    chatobserver.observe(chatRoot, {attributes: false, childList: true, characterData: false, subtree:true});
+    nameHighlightObserver.observe(chatRoot, {attributes: false, childList: true, characterData: false, subtree:true});
+    observer.disconnect();
+  }
+});
 
-  //Trade Chat Filter - New Messages
-  const chatobserver = new MutationObserver(function(mutations) {
-    if ($('input.re_chat_search').length == 1) {
-      if ($('input.re_chat_search').val() != "") {
-        var v = $('input.re_chat_search').val().toLowerCase();
-        mutations.forEach(function(mutation) {
-          if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-            if (mutation.target && mutation.target.className) {
-              if (mutation.target.className.includes('overview')) {
-                if (mutation.target.parentNode.parentNode.parentNode.className.includes("trade")) {
-                  if (mutation.addedNodes[0] && mutation.addedNodes[0].className && mutation.addedNodes[0].className.includes('message')) {
-                    if (mutation.addedNodes[0].children[1]) {
-                      var message = mutation.addedNodes[0].children[1].innerText.toLowerCase();
-                      if (!message.includes(v)) {
-                        mutation.addedNodes[0].style.display = "none";
-                      } else {
-                        mutation.addedNodes[0].style.display = "block";
-                      }
+//Trade Chat Filter - New Messages (Create Observer)
+const chatobserver = new MutationObserver(function(mutations) {
+  if ($('input.re_chat_search').length == 1) {
+    if ($('input.re_chat_search').val() != "") {
+      var v = $('input.re_chat_search').val().toLowerCase();
+      mutations.forEach(function(mutation) {
+        if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+          if (mutation.target && mutation.target.className) {
+            if (mutation.target.className.includes('overview')) {
+              if (mutation.target.parentNode.parentNode.parentNode.className.includes("trade")) {
+                if (mutation.addedNodes[0] && mutation.addedNodes[0].className && mutation.addedNodes[0].className.includes('message')) {
+                  if (mutation.addedNodes[0].children[1]) {
+                    var message = mutation.addedNodes[0].children[1].innerText.toLowerCase();
+                    if (!message.includes(v)) {
+                      mutation.addedNodes[0].style.display = "none";
+                    } else {
+                      mutation.addedNodes[0].style.display = "block";
                     }
                   }
                 }
               }
             }
           }
-        });
+        }
+      });
+    }
+  }
+});
+
+//Create highlight Observer
+const nameHighlightObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    //new messages
+    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+      if (mutation.addedNodes[0] && mutation.addedNodes[0].className) {
+        if (mutation.addedNodes[0].className.includes('message')) {
+          highlightAllNames();
+        }
+      }
+    }
+    //opening chatboxes
+    if (mutation.target && mutation.target.className) {
+      if (mutation.target.className.includes('chat-active')) {
+        highlightAllNames();
       }
     }
   });
 
+  //Already opened chats
+  if ($("#chatRoot [class*='chat-active']").length != 0) {
+    highlightAllNames();
+  }
+});
 
-  const nameHighlightObserver = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      //new messages
-      if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-        if (mutation.addedNodes[0] && mutation.addedNodes[0].className) {
-          if (mutation.addedNodes[0].className.includes('message')) {
-            highlightAllNames();
-          }
-        }
-      }
-      //opening chatboxes
-      if (mutation.target && mutation.target.className) {
-        if (mutation.target.className.includes('chat-active')) {
-          highlightAllNames();
+
+//Actually Start Observer
+observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
+
+function insertChatSearch() {
+  $('div[class^="chat-box-head"] > div[class^="chat-box-title"][title="Trade"]').append(`
+  <input type="text" class="re_chat_search" title="Filter Chat">
+  `);
+
+  $('input.re_chat_search').click(function() {
+    event.stopPropagation();
+  });
+
+  $('input.re_chat_search').on('input', function() {
+    var v = $(this).val().toLowerCase();
+    var chats = $('div[class^="chat-box"][class*="trade"]').find('div[class^="overview"] > div[class^="message"]');
+    chats.each(function() {
+      if (v == "") {
+        $(this).show();
+      } else {
+        let message = $(this).find('span').text().toLowerCase();
+        if (!message.includes(v)) {
+          $(this).hide();
+        } else {
+          $(this).show();
         }
       }
     });
+
   });
+}
 
-
-  observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
-
-
- function highlightAllNames() {
+function highlightAllNames() {
    chrome.runtime.sendMessage({name: "get_value", value: "re_settings"}, (res) => {
      if (res.status && res.status == true) {
        if (res.value.re_settings) {
@@ -120,12 +128,10 @@
        }
      }
    });
+}
 
-
-  }
-
- function removeHighlights() {
-   $('#chatRoot div[class^="message"] > a').css("color", "");
- }
+function removeHighlights() {
+ $('#chatRoot div[class^="message"] > a').css("color", "");
+}
 
 })();
