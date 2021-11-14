b991aa477981136340799cfd9618214e4a431a0f
Heasley <heasleys4hemp@hotmail.com>
2021-11-14 18:12:53 -0500

    fixed css for bs box to have rounded bottom

--- js/home/index.js
+++ js/home/index.js
@@ -90,7 +90,7 @@ if ($('#body').attr('data-traveling') != "true" && $('#body').attr('data-abroad'
       <div class="re_head expanded flat-top">
         <span class="re_title"><span class="re_logo"><span class="re_yellow">Re</span>Torn: </span><span id="re_title">Effective Stats</span></span>
       </div>
-      <div class="re_stats_list">
+      <div class="re_stats_list cont-gray bottom-round">
 
         <ul>
         <li tabindex="0" aria-label="Strength: `+stats[0].toLocaleString()+`">
