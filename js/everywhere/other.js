adcb2b908016ce1281f5409051842217d758d895
Heasley <heasleys4hemp@hotmail.com>
2021-11-14 19:15:44 -0500

    Add titles to TORN Anniversary logo

---
+++ js/everywhere/other.js
@@ -0,0 +1,15 @@
+(function() {
+  //Torn Anniversary titles
+  if ($('#tcLogo .t-letter').length != 0) {
+    $('#tcLogo .t-letter').attr("title", "+50 energy");
+  }
+  if ($('#tcLogo .o-letter').length != 0) {
+    $('#tcLogo .o-letter').attr("title", "+10 nerve");
+  }
+  if ($('#tcLogo .r-letter').length != 0) {
+    $('#tcLogo .r-letter').attr("title", "+500 happy");
+  }
+  if ($('#tcLogo .n-letter').length != 0) {
+    $('#tcLogo .n-letter').attr("title", "+25% life");
+  }
+})();
