c0edb09c05c667d3481e76dbefe14f3c0a393cc2
Heasley <heasleys4hemp@hotmail.com>
2022-01-11 00:21:13 -0500

    add visible error msg if TS error on profiles

--- js/profiles/profiles.js
+++ js/profiles/profiles.js
@@ -17,126 +17,42 @@ if ($('div.captcha').length == 0 && $('div.content-wrapper.logged-out').not('.tr
       if (res.status != undefined) {
         if (res.value.re_settings.tornstats != undefined && res.value.re_settings.tornstats == true) {
           if (res.value.re_settings.tornstats_apikey != undefined && res.value.re_settings.tornstats_apikey != "") {
-              tornstatsSync();
+              tornstatsSync()
+              .then((data) => {
+                profileHeader();
+                return data;
+              })
+              .then((data) => {
+                parseTornStatsData(data)
+              })
+              .catch((err) => {
+                $('#re_message').html(`<span class="re_error">Torn Stats Error: ${err}</span>`);
+                $('#re_message').show();
+              });
           }
         }
       }
     });
   }
 
+
   function tornstatsSync() {
-    var uid = parseInt($('a[href*="/playerreport.php?step=add&userID="]').attr("href").replace(/\D/g, ""));
-    if (uid) {
-      chrome.runtime.sendMessage({name: "pull_tornstats", selection: "spy/"+uid}, (data) => {
-        if (data) {
-          console.log(data);
-          if (data.status == true) {
-            profileHeader();
-            if (data.spy.status == true) {
-              let strCol = "";
-              let strSign = "";
-              let defCol = "";
-              let defSign = "";
-              let speCol = "";
-              let speSign = "";
-              let dexCol = "";
-              let dexSign = "";
-              let totCol = "";
-              let totSign = "";
-              if (data.spy.deltaStrength < 0) {
-                strCol = " class='red'";
-              }
-              if (data.spy.deltaStrength > 0) {
-                strCol = " class='green'";
-                strSign = "+";
-              }
-
-              if (data.spy.deltaDefense < 0) {
-                defCol = " class='red'";
-              }
-              if (data.spy.deltaDefense > 0) {
-                defCol = " class='green'";
-                defSign = "+";
-              }
-
-              if (data.spy.deltaSpeed < 0) {
-                speCol = " class='red'";
-              }
-              if (data.spy.deltaSpeed > 0) {
-                speCol = " class='green'";
-                speSign = "+";
-              }
-
-              if (data.spy.deltaDexterity < 0) {
-                dexCol = " class='red'";
-              }
-              if (data.spy.deltaDexterity > 0) {
-                dexCol = " class='green'";
-                dexSign = "+";
-              }
-
-              if (data.spy.deltaTotal < 0) {
-                totCol = " class='red'";
-              }
-              if (data.spy.deltaTotal > 0) {
-                totCol = " class='green'";
-                totSign = "+";
-              }
-
-              let spy_table = "<table><tr><th>Battle Stats</th><th>"+data.spy.player_name+"</th><th>You</th><th>Difference</th></tr>";
-              spy_table += "<tr><td>Strength:</td><td>"+data.spy.strength.toLocaleString()+"</td><td>"+Math.trunc((data.spy.strength + data.spy.deltaStrength)).toLocaleString()+"</td><td"+strCol+">"+Math.trunc(strSign+data.spy.deltaStrength).toLocaleString()+"</td></tr>";
-              spy_table += "<tr><td>Defense:</td><td>"+data.spy.defense.toLocaleString()+"</td><td>"+Math.trunc((data.spy.defense + data.spy.deltaDefense)).toLocaleString()+"</td><td"+defCol+">"+Math.trunc(defSign+data.spy.deltaDefense).toLocaleString()+"</td></tr>";
-              spy_table += "<tr><td>Speed:</td><td>"+data.spy.speed.toLocaleString()+"</td><td>"+Math.trunc((data.spy.speed + data.spy.deltaSpeed)).toLocaleString()+"</td><td"+speCol+">"+Math.trunc(speSign+data.spy.deltaSpeed).toLocaleString()+"</td></tr>";
-              spy_table += "<tr><td>Dexterity:</td><td>"+data.spy.dexterity.toLocaleString()+"</td><td>"+Math.trunc((data.spy.dexterity + data.spy.deltaDexterity)).toLocaleString()+"</td><td"+dexCol+">"+Math.trunc(dexSign+data.spy.deltaDexterity).toLocaleString()+"</td></tr>";
-              spy_table += "<tr><td>Total:</td><td>"+data.spy.total.toLocaleString()+"</td><td>"+Math.trunc((data.spy.total + data.spy.deltaTotal)).toLocaleString()+"</td><td"+totCol+">"+Math.trunc(totSign+data.spy.deltaTotal).toLocaleString()+"</td></tr>";
-              spy_table += "<tr><td>Last Spy: </td><td><b>"+data.spy.difference+"</b></td><td colspan='2'>Fair Fight Bonus: <b>x"+data.spy.fair_fight_bonus.toFixed(2)+"</b></td></tr>";
-
-              spy_table += "</table>";
-
-              $('#re_spy').html(spy_table);
-              $('#re_spy').parent().show();
-              $('#re_spy').show();
-            }
-
-            if (data.compare.status == true) {
-              let compare_table = "<table><tr><th>Personal Stats</th><th>Them</th><th>You</th></tr>";
-
-              Object.entries(data.compare.data).forEach(([key, value]) => {
-                let color = "";
-                let sign = "";
-                if (value.difference < 0) {
-                  color = " class='red'";
-                }
-                if (value.difference > 0) {
-                  color = " class='green'";
-                  sign = "+";
-                }
-                  compare_table += "<tr><td>"+key+"</td><td>"+value.amount.toLocaleString()+"</td><td"+color+">"+sign+value.difference.toLocaleString()+"</td></tr>";
-              });
-              compare_table += "<tr><td colspan='3'><a href='https://www.tornstats.com/settings/script' target='_blank'>Change your script settings here</a></td></tr>";
-              compare_table += "</table>";
-
-              $('#re_compare').html(compare_table);
-              $('#re_compare').show();
-            }
-
-            if (data.attacks.status == true) {
-              let attacks_table = "<table><tr><th>Attack History</th></tr>";
-
-              Object.entries(data.attacks.data).forEach(([key, value]) => {
-                attacks_table += "<tr><td>"+value+"</td></tr>";
-              });
-
-              attacks_table += "</table>";
-
-              $('#re_attacks').html(attacks_table);
-              $('#re_attacks').show();
-              $('#re_attacks').parent().show();
+    return new Promise((resolve, reject) => {
+      var uid = parseInt($('a[href*="/playerreport.php?step=add&userID="]').attr("href").replace(/\D/g, ""));
+      if (uid) {
+        chrome.runtime.sendMessage({name: "pull_tornstats", selection: "spy/"+uid}, (data) => {
+          if (data) {
+            if (data.status) {
+              console.log("ReTorn requesting data from Torn Stats:", data);
+              return resolve(data);
+            } else {
+              console.error(data);
+              return reject(data.message);
             }
           }
-        }
-      });
-    }
+        });
+      }
+    });
   }
 
   function profileHeader() {
@@ -150,7 +66,143 @@ if ($('div.captcha').length == 0 && $('div.content-wrapper.logged-out').not('.tr
           <div id="re_attacks" style="display: none;"></div>
         </div>
       </div>
+      <p id="re_message" style="display: none;"></p>
       `);
+  }
+
+  function parseTornStatsData(data) {
+    return new Promise((resolve, reject) => {
+      if (data.status) {
+        if (data.spy.status) {
+          let strCol = "";
+          let strSign = "";
+          let defCol = "";
+          let defSign = "";
+          let speCol = "";
+          let speSign = "";
+          let dexCol = "";
+          let dexSign = "";
+          let totCol = "";
+          let totSign = "";
+          if (data.spy.deltaStrength < 0) {
+            strCol = " class='red'";
+          }
+          if (data.spy.deltaStrength > 0) {
+            strCol = " class='green'";
+            strSign = "+";
+          }
+
+          if (data.spy.deltaDefense < 0) {
+            defCol = " class='red'";
+          }
+          if (data.spy.deltaDefense > 0) {
+            defCol = " class='green'";
+            defSign = "+";
+          }
+
+          if (data.spy.deltaSpeed < 0) {
+            speCol = " class='red'";
+          }
+          if (data.spy.deltaSpeed > 0) {
+            speCol = " class='green'";
+            speSign = "+";
+          }
+
+          if (data.spy.deltaDexterity < 0) {
+            dexCol = " class='red'";
+          }
+          if (data.spy.deltaDexterity > 0) {
+            dexCol = " class='green'";
+            dexSign = "+";
+          }
+
+          if (data.spy.deltaTotal < 0) {
+            totCol = " class='red'";
+          }
+          if (data.spy.deltaTotal > 0) {
+            totCol = " class='green'";
+            totSign = "+";
+          }
+
+          let spy_table = "<table><tr><th>Battle Stats</th><th>"+data.spy.player_name+"</th><th>You</th><th>Difference</th></tr>";
+          spy_table += "<tr><td>Strength:</td><td>"+data.spy.strength.toLocaleString()+"</td><td>"+Math.trunc((data.spy.strength + data.spy.deltaStrength)).toLocaleString()+"</td><td"+strCol+">"+Math.trunc(strSign+data.spy.deltaStrength).toLocaleString()+"</td></tr>";
+          spy_table += "<tr><td>Defense:</td><td>"+data.spy.defense.toLocaleString()+"</td><td>"+Math.trunc((data.spy.defense + data.spy.deltaDefense)).toLocaleString()+"</td><td"+defCol+">"+Math.trunc(defSign+data.spy.deltaDefense).toLocaleString()+"</td></tr>";
+          spy_table += "<tr><td>Speed:</td><td>"+data.spy.speed.toLocaleString()+"</td><td>"+Math.trunc((data.spy.speed + data.spy.deltaSpeed)).toLocaleString()+"</td><td"+speCol+">"+Math.trunc(speSign+data.spy.deltaSpeed).toLocaleString()+"</td></tr>";
+          spy_table += "<tr><td>Dexterity:</td><td>"+data.spy.dexterity.toLocaleString()+"</td><td>"+Math.trunc((data.spy.dexterity + data.spy.deltaDexterity)).toLocaleString()+"</td><td"+dexCol+">"+Math.trunc(dexSign+data.spy.deltaDexterity).toLocaleString()+"</td></tr>";
+          spy_table += "<tr><td>Total:</td><td>"+data.spy.total.toLocaleString()+"</td><td>"+Math.trunc((data.spy.total + data.spy.deltaTotal)).toLocaleString()+"</td><td"+totCol+">"+Math.trunc(totSign+data.spy.deltaTotal).toLocaleString()+"</td></tr>";
+          spy_table += "<tr><td>Last Spy: </td><td><b>"+data.spy.difference+"</b></td><td colspan='2'>Fair Fight Bonus: <b>x"+data.spy.fair_fight_bonus.toFixed(2)+"</b></td></tr>";
+
+          spy_table += "</table>";
+
+          $('#re_spy').html(spy_table);
+          $('#re_spy').parent().show();
+          $('#re_spy').show();
+        } else {
+          if (data.spy.message) {
+            if (data.spy.message.toLowerCase().includes('error')) {
+              $('#re_spy').html(`<span class="re_error"><table><tr><th>${data.spy.message}</th></tr></table></span>`);
+              $('#re_spy').show();
+              $('#re_spy').parent().show();
+            }
+          }
+        }
+
+        if (data.compare.status) {
+          let compare_table = "<table><tr><th>Personal Stats</th><th>Them</th><th>You</th></tr>";
+
+          Object.entries(data.compare.data).forEach(([key, value]) => {
+            let color = "";
+            let sign = "";
+            if (value.difference < 0) {
+              color = " class='red'";
+            }
+            if (value.difference > 0) {
+              color = " class='green'";
+              sign = "+";
+            }
+              compare_table += "<tr><td>"+key+"</td><td>"+value.amount.toLocaleString()+"</td><td"+color+">"+sign+value.difference.toLocaleString()+"</td></tr>";
+          });
+          compare_table += "<tr><td colspan='3'><a href='https://www.tornstats.com/settings/script' target='_blank'>Change your script settings here</a></td></tr>";
+          compare_table += "</table>";
+
+          $('#re_compare').html(compare_table);
+          $('#re_compare').show();
+        } else {
+          if (data.compare.message) {
+            if (data.compare.message.toLowerCase().includes('error')) {
+              $('#re_compare').html(`<span class="re_error"><table><tr><th>${data.compare.message}</th></tr></table></span>`);
+              $('#re_compare').show();
+            }
+          }
+        }
+
+        if (data.attacks.status) {
+          let attacks_table = "<table><tr><th>Attack History</th></tr>";
+
+          Object.entries(data.attacks.data).forEach(([key, value]) => {
+            attacks_table += "<tr><td>"+value+"</td></tr>";
+          });
+
+          attacks_table += "</table>";
+
+          $('#re_attacks').html(attacks_table);
+          $('#re_attacks').show();
+          $('#re_attacks').parent().show();
+        } else {
+          if (data.attacks.message) {
+            if (data.attacks.message.toLowerCase().includes('error')) {
+              $('#re_attacks').html(`<span class="re_error"><table><tr><th>${data.attacks.message}</th></tr></table></span>`);
+              $('#re_attacks').show();
+              $('#re_attacks').parent().show();
+            }
+          }
+        }
+
+      } else {
+        console.error(data);
+        return reject(data.message);
+      }
+    });
   }
 
 }
