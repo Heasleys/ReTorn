d700b28cbb8a8387817c3e7c60a747095e66d48d
Heasley <heasleys4hemp@hotmail.com>
2022-08-05 11:57:39 -0400

    update preferences to v1.0

--- js/preferences/preferences.js
+++ js/preferences/preferences.js
@@ -1,6 +1,14 @@
-// @version      1.0.0
-// @description  Add Sync button to preferences page for syncing APIKEY with extension
-// @author       Heasleys4hemp [1468764]
+(function() {
+
+//button or link click event to open options page for ReTorn
+$(document).on('click', '#re_options', function(event){
+    event.stopPropagation();
+    event.preventDefault();
+    
+    sendMessage({"name": "open_options"})
+    .catch((e) => console.error(e))
+});
+
 insertHeader($("div.content-wrapper"), 'append');
 $('#re_title').text("Sync");
 $('.re_content').html(`
@@ -9,7 +17,7 @@ $('.re_content').html(`
   </div>
   <div class="re_row">
     <div class="re_button_wrap">
-      <input id="re_apikey" type="text" maxlength="16" required></input>
+      <input id="re_apikey" type="text" maxlength="16" required autocomplete="off">
       <button class="re_torn_button" id="re_sync">Sync</button>
       <button class="re_torn_button" id="re_options">Options</button>
       <button class="re_torn_button" id="re_logout" hidden>Logout</button>
@@ -20,82 +28,84 @@ $('.re_content').html(`
   </div>
   `);
 
-
-chrome.runtime.sendMessage({name: "get_value", value: "re_api_key"}, (response) => {
-  if (response.status != undefined) {
-    if (response.status == true) {
-      synced(response);
-    } else {
-      $("#re_sync").text("Sync");
-      $("#re_sync").attr("disabled", false);
-      $("#re_logout").attr("hidden", true);
-      $("#re_apikey").show();
-    }
-  } else {
-    errorMessage({status: false, message: "Unknown error."});
-  }
-});
-
-
   $("button#re_sync").click(() => {
-    let key = $("#re_apikey").val();
-    chrome.runtime.sendMessage({name: "set_api", apikey: key}, (response) => {
-      console.log(response);
-      if (response.status != undefined) {
-        if (response.status == true) {
-          synced(response);
+    const key = $("#re_apikey").val();
+    if (key && key.length == 16) {
+        sendMessage({name: "set_api", apikey: key})
+        .then((r) => {
+            console.log(r)
+        if (r.status) {
+            isSynced(r);
         } else {
-          errorMessage(response);
+            errorMessage({status: false, message: r.message});
         }
-      } else {
-        errorMessage({status: false, message: "Unknown error."});
-      }
-    });
+        })
+        .catch((error) => errorMessage(error));
+    } else {
+        errorMessage({status: false, message: "Please enter a valid apikey."});
+    }
   });
 
   $("button#re_logout").click(function() {
-  chrome.runtime.sendMessage({name: "logout"}, (response) => {
-    console.log(response);
-    if (response.status != undefined) {
-      if (response.status == true) {
-        $("#re_sync").text("Sync");
-        $("#re_sync").attr("disabled", false);
-        $("#re_logout").attr("hidden", true);
-        $("#re_apikey").show();
-        $("p#re_signin_message").text(`You are not signed into ReTorn. Please enter your api key then click sync.`);
-        errorMessage({message: "You have been logged out."});
-      } else {
-        errorMessage(response);
-      }
-    } else {
-      errorMessage({status: false, message: "Unknown error."});
-    }
-  });
+    sendMessage({name: "logout"})
+    .then((r) => {
+        if (r.status) {
+            signedOut();
+            errorMessage({message: r.message});
+        } else {
+            errorMessage(response);
+        }
+    }).catch((error) => errorMessage(error));
 });
 
 
-function synced(response) {
-  if (response.message != undefined) {
-    $("#re_message").text(response.message);
-    $("#re_message").attr('hidden', false);
-    $("#re_message").parent().attr('hidden', false);
-  }
-  $("#re_sync").text("Synced!");
-  $("#re_sync").attr("disabled", true);
-  $("#re_sync").attr("hidden", false);
-  $("#re_logout").attr("hidden", false);
-  $("#re_apikey").hide();
-  chrome.runtime.sendMessage({name: "get_value", value: "re_user"}, (response) => {
-    console.log(response);
-    if (response.status == true && response.value.re_user.name != undefined) {
-      $("p#re_signin_message").text(`Welcome ${response.value.re_user.name}! You are signed in.`);
+
+sendMessage({name: "get_local", value: "re_apikey"})
+.then((r) => {
+    console.log(r);
+    isSynced(r);
+})
+.catch((e) => console.error(e))
+
+
+function isSynced(r) {
+ if (r.status) {
+    sendMessage({name: "get_local", value: "re_user"})
+    .then((rr) => {
+        $("#re_signin_message").text(`Welcome ${rr?.data?.name}! You are signed in.`);
+    })
+    .catch((e) => console.error(e))
+
+    if (r.message) {
+        $("#re_message").text(r.message);
+        $("#re_message").attr('hidden', false);
+        $("#re_message").parent().attr('hidden', false);
     }
-  });
+
+    $("#re_sync").text("Synced!");
+    $("#re_sync").attr("disabled", true);
+    $("#re_sync").attr("hidden", false);
+    $("#re_logout").attr("hidden", false);
+    $("#re_apikey").hide();
+ } else {
+    signedOut();
+ }
+}
+
+function signedOut() {
+    $("#re_sync").text("Sync");
+    $("#re_sync").attr("disabled", false);
+    $("#re_logout").attr("hidden", true);
+    $('#re_apikey').val('');
+    $("#re_apikey").show();
+    $("#re_signin_message").text(`You are not signed into ReTorn. Please enter your api key then click sync.`);
 }
 
 function errorMessage(error) {
-  console.log(error);
-  $("#re_message").text(error.message);
-  $("#re_message").attr('hidden', false);
-  $("#re_message").parent().attr('hidden', false);
+    console.log(error);
+    $("#re_message").text(error.message);
+    $("#re_message").attr('hidden', false);
+    $("#re_message").parent().attr('hidden', false);
 }
+
+})();
