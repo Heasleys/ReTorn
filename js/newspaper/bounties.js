27598e6c1a9790aaef66e1a3b23096e9b82a42ec
Heasley <heasleys4hemp@hotmail.com>
2022-08-06 12:49:35 -0400

    update bounties.js to v1.0

--- js/newspaper/bounties.js
+++ js/newspaper/bounties.js
@@ -1,7 +1,7 @@
 var hide_unavailable;
 
 var startupObserver = new MutationObserver(function(mutations) {
-  if ($(".bounties-wrap").length == 1 && $('div.re_container').length == 0) {
+  if ($(".bounties-wrap").length > 0 && $('div.re_container').length == 0) {
     insertHead();
     startupObserver.disconnect();
   }
@@ -36,28 +36,21 @@ function insertHead() {
     </div>
   `);
 
-  if (hide_unavailable == undefined) {
-    chrome.runtime.sendMessage({name: "get_value", value: "re_bounty"}, (response) => {
-      if (response.status) {
-        if (response.value && response.value.re_bounty) {
-          if (response.value.re_bounty.hide && response.value.re_bounty.hide.unavailable) {
-            $('#re_bounty_hide_unavailable').prop( "checked", response.value.re_bounty.hide.unavailable);
-            filterBounties(response.value.re_bounty.hide.unavailable);
-          }
-        }
-      }
-    });
-  } else {
-    filterBounties(hide_unavailable);
-  }
+  hide_unavailable = settings?.bounty?.hide?.unavailable;
+
+  filterBounties(hide_unavailable);
 
   $('#re_bounty_hide_unavailable').change(function() {
-    let checked = this.checked;
-    let name = $(this).attr('name');
-    chrome.runtime.sendMessage({name: "set_value", value_name: "re_bounty", value: {hide: {[name]: checked}}}, (response) => {
+    const checked = this.checked;
+    const name = $(this).attr('name');
+
+    const obj = {"bounty": {"hide": {"unavailable": checked}}}
+    sendMessage({"name": "merge_sync", "key": "settings", "object": obj})
+    .then((r) => {
       hide_unavailable = checked;
       filterBounties(checked);
-    });
+    })
+    .catch((e) => console.error(e))
   });
 
   $('.re_checkbox > label').click(function() {
@@ -87,8 +80,9 @@ function filterBounties(checked) {
 
 
 $(document).ready(function() {
-  if ($('div.captcha').length == 0) {
-    var target = document.querySelector('div.content-wrapper');
-    startupObserver.observe(target, {attributes: false, childList: true, characterData: false, subtree:true});
+  if (features?.pages?.bounties?.bounty_filter?.enabled) {
+    if ($('div.captcha').length == 0) {
+      startupObserver.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
+    }
   }
 });
