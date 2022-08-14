fc373db46c4486310b432288063dd7d62cf9f363
Heasley <heasleys4hemp@hotmail.com>
2022-08-14 04:28:21 -0400

    meep

--- js/profiles/heasleys4hemp.js
+++ js/profiles/heasleys4hemp.js
@@ -1,7 +1,7 @@
 // @version      1.0.0
 // @description  Speciality addition to the owner of ReTorn
 // @author       Heasleys4hemp [1468764]
-
+(function() {
 var obs = new MutationObserver(function(mutations) {
   if ($("div.user-information").length == 1) {
     let title = $('div.content-title').find('#skip-to-content').html();
@@ -12,3 +12,4 @@ var obs = new MutationObserver(function(mutations) {
 });
 
 obs.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
+})();
