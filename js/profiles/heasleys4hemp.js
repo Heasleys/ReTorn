9955ff4d75ff7cabe52eb967c09df266880558f5
Heasley <heasleys4hemp@hotmail.com>
2022-10-03 08:11:55 -0400

    turned the epic gauge to 100%

--- js/profiles/heasleys4hemp.js
+++ js/profiles/heasleys4hemp.js
@@ -3,9 +3,10 @@
 // @author       Heasleys4hemp [1468764]
 (function() {
 var obs = new MutationObserver(function(mutations) {
-  if ($("div.user-information").length == 1) {
+  if ($("div.user-information").length == 1 && $('.d .profile-wrapper .box-info .box-value .digit .two-row>span:first-child').length > 0) {
     let title = $('div.content-title').find('#skip-to-content').html();
     $('div.content-title').find('#skip-to-content').html("<span title='King of ReTorn'>👑</span> " + title);
+    $('.d .profile-wrapper .box-info .box-value .digit .two-row>span:first-child').text('Epic');
 
     obs.disconnect();
   }
