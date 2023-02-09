d45e9d2825f3be584cadca18cb3d07eaa01c9286
Heasley <heasleys4hemp@hotmail.com>
2023-02-09 15:37:20 -0500

    add auction filter functionality

---
+++ js/amarket/amarket.js
@@ -0,0 +1,381 @@
+const weaponbonuses = ["Achilles","Assassinate","Backstab","Berserk","Bleed","Blindfire","Blindside","Bloodlust","Burning","Comeback","Conserve","Cripple","Crusher","Cupid","Deadeye","Deadly","Demoralized","Disarm","Double Tap","Double-edged","Emasculate","Empower","Eviscerate","Execute","Expose","Finale","Focus","Freeze","Frenzy","Fury","Grace","Hazardous","Home Run","Irradiate","Laceration","Motivation","Paralyzed","Parry","Penetrate","Plunder","Poisoned","Powerful","Proficience","Puncture","Quicken","Rage","Revitalize","Roshambo","Severe Burning","Sleep","Slow","Smurf","Specialist","Spray","Storage","Stricken","Stun","Suppress","Sure Shot","Throttle","Toxin","Warlord","Weaken","Wind-up","Wither"]
+const armortypes = ["Riot","Assault","Dune","Delta","Marauder","EOD"];
+
+//watch for changes to auction tabs
+const auctionTabObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.target && mutation.target.className && mutation.target.className.includes('items-list')) {
+        if (mutation.addedNodes && mutation.addedNodes.length != 0) {
+            if (!mutation.addedNodes[0].className) {
+                if (mutation.target && mutation.target.className && mutation.target.className.includes('items-list')) {
+                    const tab = $('.tabContainer > div.tab-menu-cont:visible').attr('id');
+                    filter(`#${tab}`);
+                }
+            }
+        }
+      }
+    })
+});
+
+  //watch for changes to pagination
+const paginationObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.target && mutation.target.className && mutation.target.className.includes('pagination-wrap')) {
+        if (mutation.addedNodes && mutation.addedNodes.length != 0) {
+            duplicatePager();
+            paginationObserver.disconnect();
+        }
+      }
+    })
+});
+
+
+
+//check if captcha or if user is logged out first, then start function
+if ($('div.captcha').length == 0 && $('div.content-wrapper.logged-out').length == 0) { //features?.pages?.auction?.auction_filter?.enabled
+
+//watch for changes to url hash, switch filter view
+window.addEventListener('hashchange', () => {
+    const hash = location.hash;
+    
+    if (hash.includes('itemtab=weapons')) {
+        $('#re_ah_weapons').show();
+        $('#re_ah_armor').hide();
+        $('#re_ah_items').hide();  
+    }
+    if (hash.includes('itemtab=armor')) {
+        $('#re_ah_weapons').hide();
+        $('#re_ah_armor').show();
+        $('#re_ah_items').hide();     
+    }
+    if (hash.includes('itemtab=items')) {
+        $('#re_ah_weapons').hide();
+        $('#re_ah_armor').hide();
+        $('#re_ah_items').show();  
+    }
+}, false);
+
+//mutationObserver on jail wrap
+const target = document.getElementById('auction-house-tabs');
+auctionTabObserver.observe(target, {attributes: false, childList: true, characterData: false, subtree:true});
+
+const pageTarget = document.querySelector('div.pagination-wrap');
+paginationObserver.observe(target, {attributes: false, childList: true, characterData: false, subtree:true});
+
+
+//insert auction head
+insertHeader($("div#auction-house-tabs"), 'before', 'auction_filter');
+$('.re_container').after(`<hr class="delimiter-999 m-top10">`);
+
+//insert filter html
+$('.re_content').html(`
+<!-- Weapons -->
+<div id="re_ah_weapons" class="re_filter" style="display: none;">
+<p>Weapons Filter</p>
+<select id="re_ah_weapons_color"><option></option><option value="none">None</option><option value="yellow">Yellow</option><option value="orange">Orange</option><option value="red">Red</option></select>
+<input type="text" list="re_ah_weapons_list" id="re_ah_weapons_list_textbox">
+<datalist id="re_ah_weapons_list"></datalist>
+<select id="re_ah_weapons_bonuses_1"></select>
+<select id="re_ah_weapons_bonuses_2"></select>
+<input type="number" id="re_ah_weapons_damage" min="0">
+<input type="number" id="re_ah_weapons_accuracy" min="0">
+</div>
+
+<!-- Armor -->
+<div id="re_ah_armor" class="re_filter" style="display: none;">
+<p>Armor Filter</p>
+<select id="re_ah_armor_color"><option></option><option value="none">None</option><option value="yellow">Yellow</option><option value="orange">Orange</option><option value="red">Red</option></select>
+<input type="text" list="re_ah_armor_types" id="re_ah_armor_types_textbox">
+<datalist id="re_ah_armor_types">
+<option data-name="Riot" value="Riot">Impregnable</option>
+<option data-name="Assault" value="Assault">Impenetrable</option>
+<option data-name="Dune" value="Dune">Insurmountable</option>
+<option data-name="Delta" value="Delta">Invulnerable</option>
+<option data-name="Marauder" value="Marauder">Imperviable</option>
+<option data-name="EOD" value="EOD">Impassable</option>
+<option data-name="Welding Helmet" value="Welding Helmet"></option>
+<option data-name="Hazmat Suit" value="Hazmat Suit">Radiation</option>
+</datalist>
+<input type="number" id="re_ah_armor_defense" min="0">
+</div>
+
+<!-- Items -->
+<div id="re_ah_items" class="re_filter" style="display: none;">
+<p>Item Filter</p>
+<input type="text" id="re_ah_items_textbox">
+</div>
+<div class="re_row re_message">
+<p>Showing <b><span id="shown">10</span></b> out of <b><span id="total">10</span></b> people.
+</div>
+`);
+
+//fill datalist for Weapons
+var weaponlist ="<option></option>";
+$.each(weaponbonuses, function(n,e) {
+    weaponlist += '<option data-name="'+e+'">'+e+'</option>';
+});
+$('#re_ah_weapons_bonuses_1').html(weaponlist);
+$('#re_ah_weapons_bonuses_2').html(weaponlist);
+
+sendMessage({name: "get_local", value: "re_items"})
+    .then((r) => {
+        if (r.status) {
+            const items = r?.data?.items;
+            var itemsList = '<option></option>';
+            $.each(items, function(n,e) {
+                if (e?.weapon_type) {
+                    itemsList += '<option data-name="'+e?.name+'">'+e?.name+'</option>';
+                }
+            });
+            $('#re_ah_weapons_list').html(itemsList);
+        }
+    })
+    .catch((e) => console.error(e))
+
+initSearchElements();
+
+initShowFilter();
+}
+
+function duplicatePager() {
+    //duplicate pagination
+    $('.pagination-wrap').each(function(i, obj) {
+        $(this).clone().css("border-top","0").css("border-bottom", "2px solid var(--default-panel-divider-outer-side-color)").prependTo($(this).parent());
+    });
+}
+
+function initShowFilter() {
+    //check which tab is shown, then show the correct filter div
+    let itemtype = $('#auction-house-tabs .tabContainer > div.tab-menu-cont:visible').attr('data-itemtype');
+    switch (itemtype) {
+        case "weapons":
+            $('#re_ah_weapons').show();   
+        break;
+        case "armor":
+            $('#re_ah_armor').show();   
+        break;
+        case "items":
+            $('#re_ah_items').show();  
+        break;
+        default://default weapons
+            $('#re_ah_weapons').show();
+        break;
+    }
+}
+
+function initSearchElements() {
+    //Weapon Filters
+    $('#re_ah_weapons_bonuses_1, #re_ah_weapons_bonuses_2, #re_ah_weapons_color').on('change', function() {
+        filter('#types-tab-1');
+    });
+    $('#re_ah_weapons_list_textbox, #re_ah_weapons_damage, #re_ah_weapons_accuracy').on('input', function() {
+        filter('#types-tab-1');
+    });
+
+    //Armor Filters
+    $('#re_ah_armor_color').on('change', function() {
+        filter('#types-tab-2');
+    });
+    $('#re_ah_armor_types_textbox, #re_ah_armor_defense').on('input', function() {
+        filter('#types-tab-2');
+    });
+
+    //Item Filters
+    $('#re_ah_items_textbox').on('input', function() {
+        filter('#types-tab-3');
+    })
+}
+
+function nameFilter(element, searchTerm) {
+    var s1 = $(element).text().toLowerCase();//item name
+
+    if (searchTerm) {
+        if(s1.indexOf(searchTerm) !== -1) {
+            $(element).closest("li").removeClass('re_name_hide');
+        } else {
+            $(element).closest("li").addClass('re_name_hide');
+        }
+    } else {
+        $(element).closest("li").removeClass('re_name_hide');
+    }
+
+}
+
+function bonusFilter(element, b1, b2) {
+
+    //if bonus filters are not set, stop hiding based on bonus
+    if (b1 == "" && b2 == "") {
+        $(element).closest("li").removeClass("re_bonus_hide");
+        return;
+    }
+
+    //bonus elements
+    bonuses = $(element).find('.bonus-attachment-icons');
+        
+    //more than one bonus
+    if (bonuses.length == 2) {
+        const s1 = $(bonuses[0]).attr('title').toLowerCase();//bonus desc 1
+        const s2 = $(bonuses[1]).attr('title').toLowerCase();//bonus desc 2
+
+        //if b1 & b2 are not empty
+        if (b1 && b2) {//then check if bonuses exist within each bonus elements, if not hide            
+            if ((s1.indexOf(b1) !== -1 || s1.indexOf(b2) !== -1) && (s2.indexOf(b1) !== -1 || s2.indexOf(b2) !== -1)) {
+                $(element).closest("li").removeClass('re_bonus_hide');
+            } else {
+                $(element).closest("li").addClass('re_bonus_hide');
+            }
+        } else {
+            if (b1) {//if the first bonus input is set, check both bonuses to see if it exists
+                if ((s1.indexOf(b1) !== -1 || s2.indexOf(b1) !== -1)) {
+                    $(element).closest("li").removeClass('re_bonus_hide');
+                } else {
+                    $(element).closest("li").addClass('re_bonus_hide');
+                }
+            }
+            if (b2) {//now check the other bonus input for either bonus
+                if ((s1.indexOf(b2) !== -1 || s2.indexOf(b2) !== -1)) {
+                    $(element).closest("li").removeClass('re_bonus_hide');
+                } else {
+                    $(element).closest("li").addClass('re_bonus_hide');
+                }
+            }
+        }
+    }
+
+    if (bonuses.length == 1) {
+        if (b1 && b2) { //if both bonus inputs are set, then single bonus weapons should be hidden
+            $(element).closest("li").addClass('re_bonus_hide');
+            return;
+        }
+
+        const s1 = $(bonuses[0]).attr('title').toLowerCase();//bonus desc 1
+
+        if (b1) {//check the bonus based on first input
+            if (s1.indexOf(b1) !== -1) {
+                $(element).closest("li").removeClass('re_bonus_hide');
+            } else {
+                $(element).closest("li").addClass('re_bonus_hide');
+            }
+        }
+        if (b2) {//check the bonus based on 2nd input
+            if (s1.indexOf(b2) !== -1) {
+                $(element).closest("li").removeClass('re_bonus_hide');
+            } else {
+                $(element).closest("li").addClass('re_bonus_hide');
+            }
+        }
+    }
+
+    if (bonuses.length == 0) {
+        if (b1 || b2) {//if the weapons has no bonuses, but either of the bonus inputs are set, then hide
+            $(element).closest("li").addClass('re_bonus_hide');
+        }
+    }
+    
+
+}
+
+function colorFilter(element, color) {
+    if (color) {
+        if (color != "none") {
+            if ($(element).attr('class') == "item torn-item large ") {
+                $(element).closest("li").addClass("re_color_hide");
+            }
+        }
+        const colorsList = ["yellow", "orange", "red"];
+        colorsList.forEach(function(c) {
+            if ($(element).hasClass(c)) {
+                if (color != c) {
+                    $(element).closest("li").addClass("re_color_hide");
+                } else {
+                    $(element).closest("li").removeClass("re_color_hide");
+                }
+            }
+        })
+    } else {
+        $(element).closest('li').removeClass('re_color_hide');
+    }
+}
+
+function statFilter(element, value) {
+        const type = $(element).siblings('i').attr('class').replace('bonus-attachment-item-', '').replace('-bonus', '')
+        var s1 = parseFloat($(element).text());//item stat
+
+        if (value) {
+            if (s1 < value) {
+                $(element).closest("li").addClass(`re_${type}_hide`);
+            } else {
+                $(element).closest("li").removeClass(`re_${type}_hide`);
+            }
+        } else {
+            $(element).closest("li").removeClass(`re_${type}_hide`);
+        }
+}
+
+
+function filter(tab) {
+    const elements = $(`${tab} ul.items-list > li:not(.bg-blue,.clear) .item-cont-wrap`);
+
+    switch (tab) {
+        case "#types-tab-1"://weapons
+        //color
+        const colorWeapon = $('#re_ah_weapons_color').find('option:selected').val();
+
+        //name
+        const nameWeapon = $('#re_ah_weapons_list_textbox').val().toLowerCase();
+
+        //bonuses
+        const bonus_1 = $('#re_ah_weapons_bonuses_1 option:selected').attr('data-name');
+        const bonus_2 = $('#re_ah_weapons_bonuses_2 option:selected').attr('data-name');
+        const b1 = bonus_1 ? `<b>${bonus_1.toLowerCase()}</b>` : '';
+        const b2 = bonus_2 ? `<b>${bonus_2.toLowerCase()}</b>` : '';
+
+        //weapon stats
+        const damage = parseFloat($('#re_ah_weapons_damage').val());
+        const accuracy = parseFloat($('#re_ah_weapons_accuracy').val());
+
+
+        elements.each(function(index, element) {
+            colorFilter($(element).find('.img-wrap .item.torn-item'), colorWeapon);
+            nameFilter($(element).find('.title > .item-name'), nameWeapon);
+            bonusFilter($(element).find('.item-bonuses'), b1, b2);
+            //damage
+            statFilter($(element).find('.item-bonuses .infobonuses i[class="bonus-attachment-item-damage-bonus"]').siblings('span.label-value'), damage);
+            //accuracy
+            statFilter($(element).find('.item-bonuses .infobonuses i[class="bonus-attachment-item-accuracy-bonus"]').siblings('span.label-value'), accuracy);
+        });
+        break;
+
+        case "#types-tab-2"://armor
+        const colorArmor = $('#re_ah_armor_color').find('option:selected').val();
+        const nameArmor = $('#re_ah_armor_types_textbox').val().toLowerCase();
+        const defense = parseFloat($('#re_ah_armor_defense').val());
+
+        elements.each(function(index, element) {
+            colorFilter($(element).find('.img-wrap .item.torn-item'), colorArmor);
+            nameFilter($(element).find('.title > .item-name'), nameArmor);
+            statFilter($(element).find('.item-bonuses .infobonuses i[class="bonus-attachment-item-defence-bonus"]').siblings('span.label-value'), defense);
+        });
+        break;
+
+        case "#types-tab-3"://items
+        const nameItem = $('#re_ah_items_textbox').val().toLowerCase();
+        elements.each(function(index, element) {
+            nameFilter($(element).find('.title > .item-name'), nameItem);
+        });
+        break;
+    
+        default:
+            return;
+        break;
+    }
+
+    updateTotal(tab);
+}
+
+
+
+function updateTotal(parent) {
+    $('#total').text($(`${parent} ul.items-list > li:not(.clear)`).length)
+    $('#shown').text($(`${parent} ul.items-list > li:not(.clear):visible`).length)
+}
