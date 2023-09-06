ce078e12e346751f8128ad8affee09cdd51af638
Heasley <heasleys4hemp@hotmail.com>
2023-09-06 16:55:52 -0400

    wrong function name filter check

--- js/factions/memberlist.js
+++ js/factions/memberlist.js
@@ -29,7 +29,7 @@ function insertMemberListContainer() {
                 }
             }
             
-            factionMembersFilterCheck();
+            checkMemberListFilters();
     
             sendMessage({"name": "merge_sync", "key": "settings", "object": obj})
             .then((r) => {
