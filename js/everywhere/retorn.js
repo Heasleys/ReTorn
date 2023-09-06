42ae5eaa00968263a5abc502a1ee4425f54b549d
Heasley <heasleys4hemp@hotmail.com>
2023-09-06 16:09:41 -0400

    move timediff to global

--- js/everywhere/retorn.js
+++ js/everywhere/retorn.js
@@ -242,41 +242,6 @@ function secondsToHmsShort(d) {
     return dayDisplay + hDisplay + mDisplay + sDisplay;
 }
 
-function timeDifference(current, previous) {
-
-  var msPerMinute = 60 * 1000;
-  var msPerHour = msPerMinute * 60;
-  var msPerDay = msPerHour * 24;
-  var msPerMonth = msPerDay * 30;
-  var msPerYear = msPerDay * 365;
-
-  var elapsed = current - previous;
-
-  if (elapsed < msPerMinute) {
-       return Math.round(elapsed/1000) + ' seconds ago';
-  }
-
-  else if (elapsed < msPerHour) {
-       return Math.round(elapsed/msPerMinute) + ' minutes ago';
-  }
-
-  else if (elapsed < msPerDay ) {
-       return Math.round(elapsed/msPerHour ) + ' hours ago';
-  }
-
-  else if (elapsed < msPerMonth) {
-      return '' + Math.round(elapsed/msPerDay) + ' days ago';
-  }
-
-  else if (elapsed < msPerYear) {
-      return '' + Math.round(elapsed/msPerMonth) + ' months ago';
-  }
-
-  else {
-      return '' + Math.round(elapsed/msPerYear ) + ' years ago';
-  }
-}
-
 //Function to return colors darker/lighter/blended - https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
 //Version 4.0
 //This is for the secondary color in headers
