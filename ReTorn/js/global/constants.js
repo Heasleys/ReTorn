3f395f37684e4e66eac880262ef2f55cda162ce2
Heasley <heasleys4hemp@hotmail.com>
2024-03-29 16:11:41 -0400

    Add weapon category and weapon type to auction filter

--- ReTorn/js/global/constants.js
+++ ReTorn/js/global/constants.js
@@ -1,5 +1,9 @@
 const RE_ITEM_CATEGORIES = ["Alcohol","Artifact", "Book", "Booster", "Candy", "Car", "Clothing", "Collectible", "Defensive", "Drug", "Electronic", "Energy Drink", "Enhancer", "Flower", "Jewelry", "Medical", "Melee", "Other", "Plushie", "Primary", "Secondary", "Special", "Supply Pack", "Temporary", "Unused", "Virus"];
 
+const RE_WEAPON_CATEGORIES = ["Primary", "Secondary", "Melee", "Temporary"];
+
+const RE_WEAPON_TYPES = ["Clubbing", "Heavy Artillery", "Machine Gun", "Mechanical", "Piercing", "Pistol", "Rifle", "Shotgun", "Slashing", "SMG"];
+
 const RE_WEAPON_BONUSES = ["Achilles","Assassinate","Backstab","Berserk","Bleed","Blindfire","Blindside","Bloodlust","Burning","Comeback","Conserve","Cripple","Crusher","Cupid","Deadeye","Deadly","Demoralized","Disarm","Double Tap","Double-edged","Emasculate","Empower","Eviscerate","Execute","Expose","Finale","Focus","Freeze","Frenzy","Fury","Grace","Hazardous","Home Run","Irradiate","Laceration","Motivation","Paralyzed","Parry","Penetrate","Plunder","Poisoned","Powerful","Proficience","Puncture","Quicken","Rage","Revitalize","Roshambo","Severe Burning","Shock","Sleep","Slow","Smurf","Specialist","Spray","Storage","Stricken","Stun","Suppress","Sure Shot","Throttle","Toxin","Warlord","Weaken","Wind-up","Wither"]
 
 const RE_ADVANCED_ARMOR = ["Riot","Assault","Dune","Delta","Marauder","Sentinel","Vanguard","EOD"];
